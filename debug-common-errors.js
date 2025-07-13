3866e568508dc062eb22aaf0e383dc0f571904c5
Mateus Vieira <moniquefgvieira@gmail.com>
2025-07-12 19:13:54 -0500

    completed assignment

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -15,7 +15,6 @@ Debugging Steps:
 
 Reflection:
 Think about which debugging methods you found most useful and how you might apply them in future projects.
-
 */
 
 // Programs and Solutions
@@ -24,39 +23,64 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+//console.log("Welcome to the bootcamp
 
 // What’s Wrong?
 
+  // Program A is a syntax error missing closing parentheses, closing quotes, and semicolon.
+  // Fix:
+
+console.log("Welcome to the bootcamp");
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+// let numbers = [2, 4, "eight"];
+// for (let i = 0; i < numbers.length; i++) {
+//   let doubled = numbers[i] * 2;
+//   console.log(doubled);
+// }
+
+// What’s Wrong?????
+  //Runtime error--the code doesn't necessarily crash for me so much as it returns NaN when presented with the 'eight'
+  //since it's trying to do math operators on a string
+  //Fix:
+
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
-// What’s Wrong?
-
-
-
 // Program C (Logic Error)
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
+
+// function isPrime(num) {
+//   if (num < 2) return false;
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return true;  // Supposed to indicate num is NOT prime
+//     }
+//   }
+//   return false; // Supposed to indicate num IS prime
+// }
+
+// console.log(isPrime(7)); // Expected true but gets false
+
+// What’s Wrong?
+// Program C is a logic error. The true and false return statements are flipped.
+//Fix:
 
 function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
-console.log(isPrime(7)); // Expected true but gets false
-
-// What’s Wrong?
+console.log(isPrime(9));
